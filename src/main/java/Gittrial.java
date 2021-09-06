357401cea62b9f4ec66cdd24e9fc0669fe2123f2
athirakr411 <athirakr411@gmail.com>
2021-09-06 11:39:12 -0400

    Git trial add new class2

---
+++ src/main/java/Gittrial.java
@@ -0,0 +1,6 @@
+public class Gittrial {
+
+        public static void main(String[] args) {
+            System.out.println("ahgshjjjj");
+        }
+}
--- src/test/java/stepDefinitions/Gittrial.java
+++
@@ -1,7 +0,0 @@
-package stepDefinitions;
-
-public class Gittrial {
-    public static void main(String[] args) {
-        System.out.println("ahgshjjjj");
-    }
-}
